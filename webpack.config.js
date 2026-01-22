d0d867b240f911cbd5838916c200fdfa03b04bf3
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-21 17:22:43 -0700

    Update webpack.config.js with style-loader and css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,13 @@ module.exports = {
         new HtmlWepackPlugin({
             template: "./src/template.html",
         }),
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 };
