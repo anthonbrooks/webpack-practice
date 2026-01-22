eaf3626525f1eac69bb65e87f24f82905fca7ded
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-21 17:36:01 -0700

    Add image to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import rockImage from "./kevin-schmid-3060eOz9IQs-unsplash.jpg";
 
 console.log(greeting);
+
+const image = document.createElement("img");
+image.src = rockImage;
+
+document.body.appendChild(image);
